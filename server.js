bb3fb177c63c3fee84428f97fadf39edc95baea2
Stephen S. Saldana <StephenTheDev@gmail.com>
2022-08-28 15:31:38 -0700

    users route

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const connectDB = require('./config/db')
+const path = require('path')
+
+const app = express()
+
+// Connect to MongoDB
+connectDB()
+
+// init middleware
+app.use(express.json({ extended: false }))
+
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
