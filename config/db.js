b7a78c1064dd11dc1baae657f0bc31f73af45468
Stephen S. Saldana <StephenTheDev@gmail.com>
2022-08-28 19:54:49 -0700

    db.js fixed

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const db = process.env.MONGODB_URI;
+const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true
     });
 
