69ec7771f6c0cb5399eccab686b1ae3d4d133ff1
Stephen Saldana <stephenthedev1001@gmail.com>
2022-09-06 23:10:38 -0700

    CommentForm.jsx styling

--- client/src/components/comments/CommentForm.jsx
+++ client/src/components/comments/CommentForm.jsx
@@ -27,9 +27,9 @@ const CommentForm = ({ addComment, recipient }) => {
     })
   }
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className='flex flex-col p-3'>
       <textarea
-        className="border border-gray-300 rounded-lg p-2"
+        className="border border-gray-300 rounded-lg p-3"
         name="input-comment"
         id="input-comment"
         cols="30"
@@ -41,7 +41,8 @@ const CommentForm = ({ addComment, recipient }) => {
       >
 
       </textarea>
-      <input type="submit" />
+      {/* tailwindcss styling button */}
+      <input type="submit" className="bg-primary p-3" />
     </form>
   )
 }
