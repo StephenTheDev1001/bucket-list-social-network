58755821c86c98b60d231ac18b57ff9138d178fc
Stephen Saldana <stephenthedev1001@gmail.com>
2023-03-23 20:19:52 -0700

    comment out serve static

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/listItems', require('./routes/listItems'))
 app.use('/api/comments', require('./routes/comments'))
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'))
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
 
 const PORT = process.env.PORT || 5000
 
